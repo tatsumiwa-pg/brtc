710126ead30051157b437486950f9325cff34c95
tatsumiwa-pg <tatsumiwa.for.pg@gmail.com>
2022-01-16 23:26:08 +0900

    Userモデル単体テスト

---
+++ spec/factories/users.rb
@@ -0,0 +1,9 @@
+FactoryBot.define do
+  factory :user do
+    nickname { Faker::Name.name }
+    email { Faker::Internet.free_email }
+    password { Faker::Lorem.characters(number: 6, min_alpha: 1, min_numeric: 1) }
+    password_confirmation { password }
+    deleted_at { nil }
+  end
+end
