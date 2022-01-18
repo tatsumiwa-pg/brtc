14bde214ea4b214928903b7ed0bff1733f4208b6
tatsumiwa-pg <tatsumiwa.for.pg@gmail.com>
2022-01-18 18:32:38 +0900

    Userモデルのバリデーション修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  validates :nickname, presence: true, length: { minimum: 1, maximum: 40 }
+  validates :nickname, presence: true, length: { minimum: 1, maximum: 40 }, uniqueness: true
   validates :password,
             format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: 'is invalid. Include both letters and numbers' }
 end
