460a69a8f08435d420fd9b368bba40cd20d58cfd
tatsumiwa-pg <tatsumiwa.for.pg@gmail.com>
2022-01-19 15:53:36 +0900

    consultationsテーブル作成

--- db/migrate/20220119064017_create_consultations.rb
+++ db/migrate/20220119064017_create_consultations.rb
@@ -1,7 +1,12 @@
 class CreateConsultations < ActiveRecord::Migration[6.0]
   def change
     create_table :consultations do |t|
-
+      t.string     :cons_title,  null: false
+      t.integer    :category_id, null: false
+      t.string     :summary,     null: false
+      t.text       :situation,   null: false
+      t.text       :problem,     null: false
+      t.references :user,        null: false, foreign_key: true
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_15_062617) do
+ActiveRecord::Schema.define(version: 2022_01_19_064017) do
+
+  create_table "consultations", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "cons_title", null: false
+    t.integer "category_id", null: false
+    t.string "summary", null: false
+    t.text "situation", null: false
+    t.text "problem", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_consultations_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", null: false
@@ -26,4 +38,5 @@ ActiveRecord::Schema.define(version: 2022_01_15_062617) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "consultations", "users"
 end
