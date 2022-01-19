8134c4321f028a28a4f032ba2cac1cd7f65b7ddc
tatsumiwa-pg <tatsumiwa.for.pg@gmail.com>
2022-01-19 16:55:15 +0900

    バリデーションの設定

--- app/models/consultation.rb
+++ app/models/consultation.rb
@@ -2,4 +2,11 @@ class Consultation < ApplicationRecord
   belongs_to :user
   belongs_to :category
 
+  with_options presence: true do
+    :cons_title, length: { maximum: 50 }
+    :summary,    length: { maximum: 150 }
+    :situation,  length: { maximum: 2000 }
+    :problem,    length: { maximum: 2000 }
+    :category_id, numericality: { message: "can't be blank"}
+  end
 end
