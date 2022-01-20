3be9ea810b2f59ad1f8de53321aa3d5e5fb6d04c
tatsumiwa-pg <tatsumiwa.for.pg@gmail.com>
2022-01-20 22:56:33 +0900

    consultationモデル単体テスト

--- spec/factories/consultations.rb
+++ spec/factories/consultations.rb
@@ -1,5 +1,15 @@
 FactoryBot.define do
   factory :consultation do
+    cons_title  { Faker::Lorem.characters(number: 1..50) }
+    category_id { Faker::Number.between(from: 1, to: 26) }
+    summary     { Faker::Lorem.characters(number: 1..150) }
+    situation   { Faker::Lorem.characters(number: 1..2000) }
+    problem     { Faker::Lorem.characters(number: 1..2000) }
+
+    association :user
     
+    after(:build) do |consultation|
+      consultation.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
