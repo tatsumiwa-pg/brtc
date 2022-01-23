2a8c27117fb118a283acae88021dfc0777d9aa59
tatsumiwa-pg <tatsumiwa.for.pg@gmail.com>
2022-01-24 01:16:13 +0900

    createアクション修正

--- app/controllers/consultations_controller.rb
+++ app/controllers/consultations_controller.rb
@@ -14,7 +14,7 @@ class ConsultationsController < ApplicationController
   def create
     @consultation = Consultation.new(consultation_params)
     if @consultation.save
-      redirect_to consultation_path(consultation.id)
+      redirect_to consultation_path(@consultation.id)
     else
       render :new
     end
