2fb8d7188a97e6fe3e6bf076d1e81c4f6136f369
tatsumiwa-pg <tatsumiwa.for.pg@gmail.com>
2022-01-24 19:01:55 +0900

    バリデーションの設定

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -2,4 +2,9 @@ class Answer < ApplicationRecord
   belongs_to :user
   belongs_to :consultation
   has_one_attached :image
+
+  with_options presence: true do
+    validates :ans_title, length: { maximum: 40 }
+    validates :ans_text,  length: { maximum: 2000 }
+  end
 end
