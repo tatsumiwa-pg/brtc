caae94e165c76eb57a0e71c9735d4937a793c1be
tatsumiwa-pg <tatsumiwa.for.pg@gmail.com>
2022-01-28 16:40:42 +0900

    set_consultationアクションの作成

--- app/controllers/reconciliations_controller.rb
+++ app/controllers/reconciliations_controller.rb
@@ -1,14 +1,16 @@
 class ReconciliationsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_consultation
+
+
   def index
   end
 
   def new
-    @consultation = Consultation.find(params[:consultation_id])
     @reconciliation = Reconciliation.new
   end
 
   def create
-    @consultation = Consultation.find(params[:consultation_id])
     @reconciliation = Reconciliation.new(reconciliation_params)
     if @consultation.reconciliation.nil?
       @reconciliation.save
@@ -20,8 +22,14 @@ class ReconciliationsController < ApplicationController
 
   private
 
+  def set_consultation
+    @consultation = Consultation.find(params[:consultation_id])
+  end
+
   def reconciliation_params
     params[:reconciliation][:rec_text] = 'ありがとうございました' if params[:reconciliation][:rec_text].empty?
     params.require(:reconciliation).permit(:rec_text).merge(consultation_id: @consultation.id)
   end
+
+
 end
