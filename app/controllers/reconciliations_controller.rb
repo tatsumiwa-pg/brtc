a4813e011e455b23f4d409fe285c2bd165657b30
tatsumiwa-pg <tatsumiwa.for.pg@gmail.com>
2022-01-28 16:21:17 +0900

    createアクションの作成

--- app/controllers/reconciliations_controller.rb
+++ app/controllers/reconciliations_controller.rb
@@ -8,5 +8,20 @@ class ReconciliationsController < ApplicationController
   end
 
   def create
+    @consultation = Consultation.find(params[:consultation_id])
+    @reconciliation = Reconciliation.new(reconciliation_params)
+    if @consultation.reconciliation.nil?
+      @reconciliation.save
+      redirect_to consultation_path(@consultation.id)
+    else
+      redirect_to consultation_path(@consultation.id), alert: "This consultation already have reconciliation! 既に和解済みの相談です"
+    end
+  end
+
+  private
+
+  def reconciliation_params
+    params[:reconciliation][:rec_text] = 'ありがとうございました' if params[:reconciliation][:rec_text].empty?
+    params.require(:reconciliation).permit(:rec_text).merge(consultation_id: @consultation.id)
   end
 end
