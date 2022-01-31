068269ed2b158a6f0b2469b95db5206a5aa70c1b
tatsumiwa-pg <tatsumiwa.for.pg@gmail.com>
2022-01-31 23:49:18 +0900

    conscommentモデル単体テスト

--- spec/factories/cons_comments.rb
+++ spec/factories/cons_comments.rb
@@ -1,5 +1,8 @@
 FactoryBot.define do
   factory :cons_comment do
-    
+    cons_c_text { Faker::Lorem.characters(number: 1..150) }
+
+    association :user
+    association :consultation
   end
 end
