1ab86b40313133e7ac1af1c5a3526eaea9d61441
tatsumiwa-pg <tatsumiwa.for.pg@gmail.com>
2022-02-02 16:55:03 +0900

    createアクションの作成/ルーティングのネスト

--- app/controllers/ans_comments_controller.rb
+++ app/controllers/ans_comments_controller.rb
@@ -1,2 +1,17 @@
 class AnsCommentsController < ApplicationController
+  before_action :authenticate_user!
+
+  def create
+    @answer = Answer.find(params[:answer_id])
+    @ans_comment = AnsComment.new(ans_comment_params)
+    if @ans_comment.save
+      redirect_to answer_path(@answer.id)
+    end
+  end
+
+  private
+
+  def ans_comment_params
+    params.require(:ans_comment).permit(:ans_c_text).merge(user_id: current_user.id, answer_id: @answer.id)
+  end
 end
