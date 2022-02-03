bd7a4cb6075bdffaf2b2989568da0806e6d297f3
tatsumiwa-pg <tatsumiwa.for.pg@gmail.com>
2022-02-03 22:20:47 +0900

    コメントの一覧表示機能実装

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -24,6 +24,7 @@ class AnswersController < ApplicationController
     @answer = Answer.find(params[:id])
     @consultation = Consultation.find_by(id: @answer.consultation_id)
     @ans_comment = AnsComment.new
+    @ans_comments = AnsComment.where(answer_id: @answer.id).includes(:user).order('created_at DESC')
   end
 
   private
