e88458b5aceb7d1ac7a6bd1d758588691d5bf605
tatsumiwa-pg <tatsumiwa.for.pg@gmail.com>
2022-02-08 22:57:09 +0900

    reviewモデル単体テスト

--- spec/factories/reviews.rb
+++ spec/factories/reviews.rb
@@ -1,5 +1,8 @@
 FactoryBot.define do
   factory :review do
-    
+    point { rand(4) + 1 }
+
+    association :user
+    association :answer
   end
 end
--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -1,5 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe Review, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @review = FactoryBot.build(:review)
+  end
+
+  context '評価情報の入力内容に問題がない場合' do
+    it '項目が正しく入力されていれば回答を投稿できる' do
+      expect(@review).to be_valid
+    end
+  end
+
+  context '評価情報の入力内容に問題がある場合' do
+    it 'pointがカラではユーザー登録できない' do
+      @review.point = ''
+      @review.valid?
+      expect(@review.errors.full_messages).to include("Point can't be blank")
+    end
+    it 'userが存在しなければ登録できない' do
+      @review.user = nil
+      @review.valid?
+      expect(@review.errors.full_messages).to include('User must exist')
+    end
+    it 'answerが存在しなければ登録できない' do
+      @review.answer = nil
+      @review.valid?
+      expect(@review.errors.full_messages).to include('Answer must exist')
+    end
+  end
 end
