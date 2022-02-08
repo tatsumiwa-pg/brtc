79692033e7c82437d47fccef66178edb55af7295
tatsumiwa-pg <tatsumiwa.for.pg@gmail.com>
2022-02-08 23:50:00 +0900

    seedファイル修正2

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,7 @@
-User.create(id: 1,	nickname: 'test',	email: 'test@test.com',	  password: '11111q')
+User.create(nickname: 'test',	email: 'test@test.com',	  password: '11111q')
 
 4.times do |i|
   User.create(
-    id:        i + 2,
     nickname:  "test#{i + 2}",
     email:     "test#{i + 2}@test.com",
     password:  '11111q'
@@ -11,7 +10,6 @@ end
 
 20.times do |i|
   Consultation.create(
-    id:          i + 1,
     cons_title:  Faker::Lorem.characters(number: 1..50),
     category_id: Faker::Number.between(from: 1, to: 26),
     summary:     Faker::Lorem.characters(number: 1..150),
@@ -23,7 +21,6 @@ end
 
 100.times do |i|
   Answer.create(
-    id:              i + 1,
     ans_title:       Faker::Lorem.characters(number: 1..40),
     ans_text:        Faker::Lorem.characters(number: 1..2000),
     user_id:         Faker::Number.between(from: 4, to: 5),
@@ -33,7 +30,6 @@ end
 
 10.times do |i|
   Reconciliation.create(
-    id:              i + 1,
     rec_text:        Faker::Lorem.characters(number: 1..40),
     consultation_id: i + 1
   )
@@ -42,7 +38,6 @@ end
 
 100.times do |i|
   ConsComment.create{
-    id:              i + 1,
     cons_c_text:     Faker::Lorem.characters(number: 1..150),
     consultation_id: Faker::Number.between(from: 1, to: 17)
   }
@@ -50,8 +45,7 @@ end
 
 200.times do |i|
   AnsComment.create{
-    id:              i + 1,
-    ans_c_text:     Faker::Lorem.characters(number: 1..150),
-    answer_id: Faker::Number.between(from: 1, to: 50)
+    ans_c_text: Faker::Lorem.characters(number: 1..150),
+    answer_id:  Faker::Number.between(from: 1, to: 50)
   }
 end
