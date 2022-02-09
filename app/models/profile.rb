b3283b532dee517b854c1ca96af2a9dc0378db96
tatsumiwa-pg <tatsumiwa.for.pg@gmail.com>
2022-02-09 16:45:01 +0900

    バリデーションとアソシエーションの設定

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,2 +1,19 @@
 class Profile < ApplicationRecord
+  belongs_to :user
+  has_one_attached :user_image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :age
+  belongs_to :family_type
+  belongs_to :house_env
+
+  with_options presence: true do
+    validates :age_id, :family_type_id, :house_env_id
+    validates :job,       length: { maximum: 100 }
+    validates :skills,    length: { maximum: 200 }
+    validates :address,   length: { maximum: 50 }
+    validates :cat_exp,   length: { maximum: 200 }
+    validates :my_cats,   length: { maximum: 500 }
+    validates :introduce, length: { maximum: 1000 }
+  end
 end
