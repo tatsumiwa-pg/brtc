246ab5b0325a0ce0e64da3a9e4eeec741dcca97f
tatsumiwa-pg <tatsumiwa.for.pg@gmail.com>
2022-02-10 01:53:20 +0900

    newアクションの作成/cssファイル整理

--- config/routes.rb
+++ config/routes.rb
@@ -12,4 +12,5 @@ Rails.application.routes.draw do
     resources :reconciliations, only: [:index, :new, :create]
     resources :cons_comments, only: :create
   end
+  resources :profiles, only: [:new]
 end
