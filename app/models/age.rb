cbd658aa42e2b336c66477180a0c8498c6ce9903
tatsumiwa-pg <tatsumiwa.for.pg@gmail.com>
2022-02-10 20:09:34 +0900

    ActiveHashにデフォルトidを追加

--- app/models/age.rb
+++ app/models/age.rb
@@ -1,10 +1,11 @@
 class Age < ActiveHash::Base
   self.data = [
-    { id:  1, name: '~17' },
-    { id:  2, name: '~29' },
-    { id:  3, name: '~39' },
-    { id:  4, name: '~49' },
-    { id:  5, name: '50~' }
+    { id:  1, name: '未選択' },
+    { id:  2, name: '~17' },
+    { id:  3, name: '~29' },
+    { id:  4, name: '~39' },
+    { id:  5, name: '~49' },
+    { id:  6, name: '50~' }
   ]
 
   include ActiveHash::Associations
