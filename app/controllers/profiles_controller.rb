44d2df5cfe07bf28bc651eabcb34e28ec03159d4
tatsumiwa-pg <tatsumiwa.for.pg@gmail.com>
2022-02-12 19:55:10 +0900

    createアクションの場合分け
    - カラムが全てカラなら保存しない
    - １つでも項目が埋まっていれば保存

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -11,25 +11,47 @@ class ProfilesController < ApplicationController
   end
 
   def create
-    @profile = Profile.new(profile_params)
-    if @profile.save
-      redirect_to root_path
-    else
-      render :new
+    if check_params
+      @profile = Profile.new(profile_params)
+      if @profile.save
+        redirect_to root_path and return
+      else
+        render :new
+      end
     end
+    redirect_to root_path
   end
 
   private
 
-  def profile_params
-    params[:profile][:job] = '未記入' if params[:profile][:job].blank?
-    params[:profile][:skills] = '未記入' if params[:profile][:skills].blank?
-    params[:profile][:address] = '未記入' if params[:profile][:address].blank?
-    params[:profile][:cat_exp] = '未記入' if params[:profile][:cat_exp].blank?
-    params[:profile][:my_cats] = '未記入' if params[:profile][:my_cats].blank?
-    params[:profile][:introduction] = '未記入' if params[:profile][:introduction].blank?
+  def check_params
+    @params = params[:profile]
+    ids = [@params[:age_id], @params[:family_type_id], @params[:house_env_id]]
+    others = [
+      @params[:job],
+      @params[:skills],
+      @params[:address],
+      @params[:cat_exp],
+      @params[:my_cats],
+      @params[:introduction],
+      @params[:user_image],
+    ]
 
-    binding.pry
+    if ids.all? {|id| id == '1'}
+      others.all? {|other| other.blank?} ? false : true
+    else
+      true
+    end
+  end
+
+  def profile_params
+    @params[:job] = '未記入' if @params[:job].blank?
+    @params[:skills] = '未記入' if @params[:skills].blank?
+    @params[:address] = '未記入' if @params[:address].blank?
+    @params[:cat_exp] = '未記入' if @params[:cat_exp].blank?
+    @params[:my_cats] = '未記入' if @params[:my_cats].blank?
+    @params[:introduction] = '未記入' if @params[:introduction].blank?
+
     params.require(:profile).permit(
       :age_id,
       :job,
